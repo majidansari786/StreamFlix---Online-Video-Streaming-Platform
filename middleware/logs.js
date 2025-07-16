a4d90126b2d87abf125947033150e8556ca6e8c3
Majid Ansari <MAJID_ANSARI24@manavrachna.net>
2025-07-16 10:03:38 +0530

    Update logs.js

--- middleware/logs.js
+++ middleware/logs.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const date = require('date-fns');
 
-if(!fs.existsSync('logs')){
-    fs.mkdirSync('logs');
-};
+// if(!fs.existsSync('logs')){
+//     fs.mkdirSync('logs');
+// };
 
 function logs(method,url,origin,userip){
     const datetime = `${date.format(new Date(),'yyyy-MM-dd\tHH:mm:ss')}`;
     const data = `\n${datetime} ${method} at ${url} by ${origin} with ip ${userip}`;
-    fs.appendFileSync(path.join('.','logs','logs.txt'),data);
+    // fs.appendFileSync(path.join('.','logs','logs.txt'),data);
 };
 
 module.exports = logs;
